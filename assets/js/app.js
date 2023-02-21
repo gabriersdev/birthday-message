ba1821e428b218266e061a4f77520006f1f2d2c5
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-21 18:13:34 -0300

    Add. arquivos e códigos-base para o desenvolvimento

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+"use strict"
+
+(() => {
+
+  document.querySelectorAll('[data-recarrega-pagina]').forEach(botao => {
+    botao.addEventListener('click', () => {
+      window.location.reload;
+    })
+  })
+
+})();
