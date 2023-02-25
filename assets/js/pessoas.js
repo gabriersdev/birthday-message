12f90128ef4898efbf324886c64f0212660ad820
Gabries <105074547+gabrieszin@users.noreply.github.com>
2023-02-25 16:08:52 -0300

    Alterado o nome

--- assets/js/pessoas.js
+++ assets/js/pessoas.js
@@ -2,7 +2,7 @@ export const pessoas = {
   dados: [
     {nome: 'Exemplo', dataNasc: '2023-01-01'},
     {nome: 'Gabriel', dataNasc: '2004-01-27'},
-    {nome: 'João', dataNasc: '2004-04-21'},
+    {nome: 'Jão', dataNasc: '2004-04-21'},
     {nome: 'Isabella', dataNasc: '2004-04-13'},
     {nome: 'Leticia', dataNasc: '2006-10-25'},
     {nome: 'Arthur', dataNasc: '2004-03-25'},
